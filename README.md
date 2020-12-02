719fa3af87674fe1ccb36a6cf881a1a397c3113b
hx <huangxin@wondertek.com.cn>
2020-12-02 18:44:22 +0800

    app1

--- README.md
+++ README.md
@@ -1,10 +1,35 @@
-# express-demo
+### express
 
-## app1.js
+### app1.js
 
 调用 `express`， 创建web应用
 
-``` js
+```js
 const express = require('express')
-
 const app = express()
+```
+
+指定外部访问端口，默认8080，启动应用，输出~
+
+```js
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log('Magic hapens on port ', port)
+```
+
+创建一个路由对象router，指定访问`/hello`时，页面输出`Hello World`
+
+`app.use`将路由router加载到应用的 `/home`路径，即访问`/home/hello`，返回`Hello World`
+
+```js
+const router = express.Router()
+
+router.get('/hello', (req, res) => {
+  res.send(`<h1>Hello World</h1>`)
+})
+
+app.use('/home', router)
+```
+
+
+
--- app1.js
+++ app1.js
@@ -1,27 +1,9 @@
-### express
-
-### app1.js
-
-调用 `express`， 创建web应用
-
-```js
 const express = require('express')
+
 const app = express()
-```
 
-指定外部访问端口，默认8080，启动应用，输出~
-
-```js
 const port = process.env.PORT || 8080
-app.listen(port)
-console.log('Magic hapens on port ', port)
-```
 
-创建一个路由对象router，指定访问`/hello`时，页面输出`Hello World`
-
-`app.use`将路由router加载到应用的 `/home`路径，即访问`/home/hello`，返回`Hello World`
-
-```js
 const router = express.Router()
 
 router.get('/hello', (req, res) => {
@@ -29,7 +11,7 @@ router.get('/hello', (req, res) => {
 })
 
 app.use('/home', router)
-```
 
+app.listen(port)
 
-
+console.log('Magic hapens on port ', port)
